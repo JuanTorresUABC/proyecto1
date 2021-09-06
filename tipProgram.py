17ac9b4759c422804e0e2a15686534f2e1720e3e
JuanTorresUABC <89890741+JuanTorresUABC@users.noreply.github.com>
2021-09-06 01:24:26 -0700

    Programa para calcular propina
    Lo hice de la manera simple, espero aprender a hacer la manera complicada

---
+++ tipProgram.py
@@ -0,0 +1,9 @@
+print("Calculador de propina")
+precioTotal = float(input("Cual es la cuenta total de hoy? "))
+propina = precioTotal * 1.18
+
+print("La propina de una persona es de: "+str(propina))
+
+
+    
+
