e71f85cd10dfc1bb36089764e4a9beb4b8447b4f
JuanTorresUABC <89890741+JuanTorresUABC@users.noreply.github.com>
2021-09-06 00:58:42 -0700

    Juego de generador de una historia con palabras

---
+++ madLibs.py
@@ -0,0 +1,8 @@
+print('Bienvenido, vamos a crear tu historia')
+parteCuerpo= input('Escribe una parte del cuerpo: ')
+adjetivo= input('Escribe un adjetivo: ')
+trabajo= input('Escribe un trabajo: ')
+animal= input('Escribe un animal: ')
+comida= input('Escribe una comida: ')
+
+print('Pensarias que la isla de la felicidad es el mejor lugar para relajarse y limpiar tu '+ parteCuerpo+' ,pero parece que algo '+ adjetivo +' siempre sucede, como cuando tu mejor amigo se viste de '+trabajo+' y visita a el '+animal+' para hacer un delicioso '+comida+'.')
