c81dbba2a937ef029fd93f8e20855015c9a57a27
JuanTorresUABC <89890741+JuanTorresUABC@users.noreply.github.com>
2021-09-06 00:59:15 -0700

    Programa para saber si un numero es par o impar

---
+++ oddEven.py
@@ -0,0 +1,7 @@
+print('Bienvenido, piensa en un numero por favor')
+number = int(input('Que numero estas pensando?\n'))
+
+if(number%2==0):
+    print('Tu numero es par')
+else:
+    print('Tu numero es impar')
